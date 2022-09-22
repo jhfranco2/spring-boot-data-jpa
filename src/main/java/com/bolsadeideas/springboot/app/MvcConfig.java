61fc4fa536223795e38ff67100c1909dbf45deab
jfranco <jhoanmateofranco@gmail.com>
2022-09-22 08:10:22 -0500

    seguridad en memoria con boton de sign in

--- src/main/java/com/bolsadeideas/springboot/app/MvcConfig.java
+++ src/main/java/com/bolsadeideas/springboot/app/MvcConfig.java
@@ -7,6 +7,7 @@ package com.bolsadeideas.springboot.app;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -27,6 +28,8 @@ public class MvcConfig implements WebMvcConfigurer {
 		*/
 	}
 	
-	
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/error_403").setViewName("error_403");
+	}
 
 }
